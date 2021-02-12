e2cd3c0e12a1c2506574f95516f7539f9997b634
dhul-husni <sir3n.sn@gmail.com>
2021-02-12 04:55:05 +0300

    add referral_link to user data

--- app/serializers/api/v1/base_serializer.rb
+++ app/serializers/api/v1/base_serializer.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
-
 class Api::V1::BaseSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   def created_at
     object.created_at.to_datetime.in_time_zone('UTC').iso8601 if object.created_at
   end
--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -4,4 +4,9 @@ class Api::V1::UserSerializer < Api::V1::BaseSerializer
   attributes(*User.attribute_names - %w[created_at updated_at password_digest])
 
   attribute :total_rewarded_amount
+  attribute :referral_link
+
+  def referral_link
+    api_v1_signup_path(referral_code: object.referral_code)
+  end
 end
